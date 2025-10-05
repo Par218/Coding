229aaa1cebaea1c1d292225eac76228a44b6742d
Par218 <u19032936@tuks.co.za>
2025-10-05 21:02:53 +0200

    with graphs

--- barChart.js
+++ barChart.js
@@ -1,18 +1,26 @@
 /* data */
 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
-const datas_array=[65, 59, 80, 81, 56, 55];
+const sales_array=[65, 59, 80, 81, 56, 55];
+const Production_array=[28, 48, 40, 19, 86, 27];
 
 /* chart creation */
 const myChart = new Chart('myChart',{
     type : 'bar',
     data: {
         labels: months,
-        datasets: [{
+        datasets: [
+            {
             label: 'Sales',
             backgroundColor: 'blue',
-            data
-        }]
+            data: sales_array
+            },
+            {
+            label: 'Production',
+            backgroundColor: 'orange',
+            data: Production_array
+            }
+        ]
     }
 
 })
